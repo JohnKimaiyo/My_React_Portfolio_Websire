725482bda47f16703da1bdcbb652060b0b60a4bd
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-12-27 10:31:41 +0300

    about page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Contact from "./Components/Contact";
+import Projects from "./Components/Projects";
+import Testimonials from "./Components/Testimonials";
+import Navbar from "./Components/Navbar";
+import Skills from "./Components/Skills";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="text-gray-400 bg-gray-900 body-font">
+      <Contact />
+      <Navbar />
+      <Projects />
+      <Skills />
+      <Testimonials />
+    </main>
   );
 }
 
